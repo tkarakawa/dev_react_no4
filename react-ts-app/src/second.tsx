27baa5cb3836e2aeedf34b41fb86840122d7bab6
tKarakawa <takayuki_karakawa@customatrix.co.jp>
2022-01-05 12:12:08 +0900

    20110105　いったんコミット

--- react-ts-app/src/second.tsx
+++ react-ts-app/src/second.tsx
@@ -3,15 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
     <App />
-    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
